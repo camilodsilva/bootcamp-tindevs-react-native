3fe98203aa688b55e5f19823f10a328b7d5ac4f9
CamiloSilvaD <camilodsmp@gmail.com>
2020-04-03 08:05:30 -0300

    Initial commit

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
+
+import Login from "./pages/Login";
+import Main from "./pages/Main";
+
+export default createAppContainer(
+  createSwitchNavigator({
+    Login,
+    Main
+  })
+);
+
+// createMaterialTopTabNavigator
+// createStackNavigator
+// createBottomTabNavigator
+// createDrawerNavigator
